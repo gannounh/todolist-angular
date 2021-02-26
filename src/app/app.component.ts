72723a948a3c4ea8de1b2bd4c5f0349b3cf94b97
Hicham GANNOUNE <gannoune.hicham@gmail.com>
2021-02-26 11:55:37 +0100

    version 1 is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { strToTdl, TodolistService, TodoItem } from './todolist.service';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,29 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {
+  constructor(private tdl:TodolistService) {
     /* Ajoutez un paramètre de type TodolistService au constructeur */
+
+  }
+  get obSListe(){
+    return this.tdl.observable;
   }
 
+  ajouterTache(t:string):void{
+    this.tdl.append(t);
+  }
+  supprimerTache(t:TodoItem):void{
+    this.tdl.remove(t);
+    console.log(t);
+  }
+  updateTache(label: string, item:TodoItem): void{
+    this.tdl.update({label}, item);
+  }
+  do(item:TodoItem){
+    if(item.isDone){
+      this.tdl.update({isDone: false}, item);
+    }else{
+      this.tdl.update({isDone: true}, item);
+    }
+  }
 }
